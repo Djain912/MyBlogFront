15002d6c20b34942f40527890a176ffe6d3e10c4
Djain912 <darshan_arvind.jain@vpt.edu.in>
2024-08-08 14:28:47 +0530

    cmt

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,7 +14,7 @@ export default function Home() {
   <div className="hero-overlay bg-opacity-60"></div>
   <div className="hero-content text-neutral-content text-center">
     <div className="max-w-md">
-      <h1 className="mb-5 text-5xl font-bold">Hello, Coders</h1>
+      <h1 className="mb-5 text-5xl font-bold">Hello Coders</h1>
       <p className="mb-5">
         Documenting my coding experiences and solutions to real-world problems.
       </p>
