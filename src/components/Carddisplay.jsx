b89332a2a497b7bfd410fd9a6fa7a8af20058fdf
Djain912 <darshan_arvind.jain@vpt.edu.in>
2024-08-09 10:50:46 +0530

    cmt

--- src/components/Carddisplay.jsx
+++ src/components/Carddisplay.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import Card from '../components/Card';
+import Loader from '../components/Loader';
 
 export default function Carddisplay() {
   const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true); // State to track loading
 
   useEffect(() => {
     const fetchArticles = async () => {
@@ -10,8 +12,10 @@ export default function Carddisplay() {
         const response = await fetch('https://myblogbackend-fk9u.onrender.com/getrecentposts');
         const data = await response.json();
         setArticles(data.slice(0, 10)); // Fetching up to 10 most recent posts
+        setLoading(false); // Set loading to false after data is fetched
       } catch (error) {
         console.error('Error fetching articles:', error);
+        setLoading(false); // Set loading to false even if there's an error
       }
     };
 
@@ -19,16 +23,20 @@ export default function Carddisplay() {
   }, []);
 
   return (
-    <div className="flex flex-wrap justify-center ">
-      {articles.map((article, index) => (
-        <Card 
-          key={index} 
-          title={article.title} 
-          subtitle={article.subtitle} 
-          imgSrc={article.image} 
-          link={`/post/${article._id}`}
-        />
-      ))}
+    <div className="flex flex-wrap justify-center">
+      {loading ? (
+        <Loader /> // Show loader if loading
+      ) : (
+        articles.map((article, index) => (
+          <Card 
+            key={index} 
+            title={article.title} 
+            subtitle={article.subtitle} 
+            imgSrc={article.image} 
+            link={`/post/${article._id}`}
+          />
+        ))
+      )}
     </div>
   );
 }
